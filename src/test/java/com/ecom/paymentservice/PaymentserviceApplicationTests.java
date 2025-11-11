9928a2cf660d13c438d66ec270f2c611b723bf2e
shann606 <shann606@gmail.com>
2025-11-11 14:39:12 +0530

    First commit

---
+++ src/test/java/com/ecom/paymentservice/PaymentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
